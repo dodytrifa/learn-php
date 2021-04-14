bc1ac90dff21c2b81009623be2d9a9ccfc83d5dd
dodytrifa <dody.trifa@gmail.com>
2021-04-14 20:43:19 +0700

    array

--- tes.php
+++ tes.php
@@ -1,6 +1,6 @@
 <?php
 
-// echo "Hello Dody";
+echo "Hello Dody";
 
 echo "Decimal : ";
 var_dump(1234);
@@ -19,3 +19,42 @@ var_dump(1234_2345435_2343);
 
 echo "Floating point : ";
 var_dump(1.234);
+
+echo "Benar : ";
+var_dump(true);
+
+echo 'Salah : ';
+var_dump(false);
+
+echo <<<Heredoc
+ini untuk buat paragraf yang multiline 
+panjang sekali bos
+Heredoc;
+echo "\n";
+
+//* VARIABLE
+
+//* $name = "John"
+
+//? CONSTANT
+
+define("AUTHOR", "Yuval"); //! karena constant, gak boleh define lagi di bawah
+define("BOOKS", "SAPIENS");
+
+echo AUTHOR;
+echo "\n";
+echo BOOKS;
+
+//* NULL
+$name = "John";
+echo "Is name null : ";
+
+var_dump(is_null($name)); //* name null : bool(false)
+
+//? UNSET: untuk menghapus variabel
+$variabel = "John";
+unset($variabel);
+
+//* jika di echo akan error di terminal
+
+//? ISSET: cek data null
