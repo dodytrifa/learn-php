6108c46a08244920086ff6642bb5a682760d4c30
dodytrifa <dody.trifa@gmail.com>
2021-04-14 20:09:52 +0700

    number

--- tes.php
+++ tes.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "Hello Dody";
+// echo "Hello Dody";
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b11111);
+
+echo "Underscore in number : ";
+var_dump(1234_2345435_2343);
+
+echo "Floating point : ";
+var_dump(1.234);
